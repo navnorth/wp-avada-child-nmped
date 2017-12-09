7c83a22cb05b8ef190cc07ce4e578ecafd09ae24
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-12-09 21:45:56 +0800

    add video(youtube) embed shortcode to WYSIWYG Editor

--- theme-functions/tinymce_button/popup_generator.php
+++ theme-functions/tinymce_button/popup_generator.php
@@ -1,4 +1,8 @@
 <?php
+/** Load WordPress Bootstrap */
+$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+require_once( $parse_uri[0] . 'wp-load.php' );
+
 extract($_REQUEST);
 if($action == "show_popup")
 {
@@ -13,6 +17,22 @@ if($action == "show_popup")
                                     </div>
                                     <div class="nmped_snglttltiny">
                                             Bootstrap Grid
+                                    </div>
+                                </div>
+                                <div class="nmped_sngltinyclm" onclick="nmped_clicked(this);" data-shortcode="spacer">
+                                    <div class="nmped_snglimgtiny nmped_spacer">
+                                            <hr class="nmped_top35" />
+                                    </div>
+                                    <div class="nmped_snglttltiny">
+                                            Spacer
+                                    </div>
+                                </div>
+                                <div class="nmped_sngltinyclm" onclick="nmped_clicked(this);" data-shortcode="embed_video">
+                                    <div class="nmped_snglimgtiny">
+                                        <img src="'.get_stylesheet_directory_uri().'/theme-functions/tinymce_button/images/featured_video.png">
+                                    </div>
+                                    <div class="nmped_snglttltiny">
+                                        Embed Video
                                     </div>
                                 </div>
                             </div>
@@ -49,10 +69,16 @@ if($action == "show_popup")
 			{
 				switch (shortcode_type)
 				{
-				   case "bsgrid":
+                                    case "bsgrid":
 					   var shortcode = "[row][column md=\'4\'] your 1st column content here[/column][column md=\'4\'] your 2nd column content here[/column][column md=\'4\'] your 3rd column content here[/column][/row]";
 					   break;
-				   default:
+                                    case "spacer":
+					   var shortcode = "[spacer height=\'16\']";
+					   break;
+                                    case "embed_video":
+                                            var shortcode = "[fusion_youtube id=\'\' alignment=\'\' width=\'\' height=\'\' autoplay=\'false\' api_params=\'\' hide_on_mobile=\'large-visibility\' class=\'\'][/fusion_youtube]";
+                                            break;
+                                    default:
 				   	   var shortcode = "";
 				   	   break
 				}
