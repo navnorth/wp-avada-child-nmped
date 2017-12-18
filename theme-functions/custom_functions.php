4ae8f5b2b2c44bf8a1a78341cc5f6de56931d285
Sandeep Yadav <sandeep.yadav@monadinfotech.com>
2017-12-18 15:42:14 +0530

    # 7617258 - No Tab to Search --Added tabindex in custom_functions file, and css for focus fixes when tabbed

--- theme-functions/custom_functions.php
+++ theme-functions/custom_functions.php
@@ -10,15 +10,15 @@ if ( ! function_exists( 'nmped_display_sidenav' ) ) {
 
 		if ( is_page_template( 'side-navigation.php' ) && 0 !== get_queried_object_id() ) {
 			$html = '<ul class="side-nav">';
-			
+
 			$parent = false;
 			$post_ancestors = get_ancestors( $post_id, 'page' );
 			$post_parent    = reset( $post_ancestors );
 			$post_children = wp_list_pages( 'title_li=&child_of=' . $post_id . '&echo=0' );
-			
+
 			if (count($post_ancestors)<=1 && $post_children)
 				$parent = true;
-			
+
 			$html .= ( is_page( $post_parent ) || $parent ) ? '<li class="current_page_item">' : '<li>';
 
 			if (count($post_ancestors)>1){
@@ -56,44 +56,44 @@ if ( ! function_exists( 'nmped_display_subpages' ) ) {
 			$html = "";
 			$parent = false;
 			$children = "";
-	
+
 			$args = array(
 				'post_parent' => $post_id,
-				'post_type'   => 'page', 
+				'post_type'   => 'page',
 				'numberposts' => -1,
-				'post_status' => 'publish' 
+				'post_status' => 'publish'
 			);
 			$post_children = get_children( $args );
-			
+
 			$post_ancestors = get_ancestors( $post_id, 'page' );
 			$post_parent    = reset( $post_ancestors );
-			
+
 			if (($post_children || count($post_ancestors)>1) && $post_parent) {
-	
+
 				$html = '<ul class="side-nav side-nav-widget">';
-				
+
 				if (count($post_ancestors)<=1 && $post_children)
 					$parent = true;
-				
+
 				$html .= ( is_page( $post_parent ) || $parent ) ? '<li class="current_page_item">' : '<li>';
-	
+
 				if (count($post_ancestors)>1){
 					$top_level = array_pop($post_ancestors);
 					$post_ancestors = array_reverse($post_ancestors);
-					
+
 					$grand_ancestor = $post_ancestors[0];
-					
+
 					$html    .= '<a href="' . get_permalink( $grand_ancestor ) . '" title="' . esc_html__( 'Back to Parent Page', 'Avada' ) . '">' . get_the_title( $grand_ancestor ) . '</a></li>';
 					$children .= wp_list_pages( 'title_li=&child_of=' . $grand_ancestor . '&echo=0' );
 				} else {
 					$html    .= '<a href="' . get_permalink( $post_id ) . '" title="' . esc_html__( 'Back to Parent Page', 'Avada' ) . '">' . get_the_title( $post_id ) . '</a></li>';
 					$children = wp_list_pages( 'title_li=&child_of=' . $post_id . '&echo=0' );
 				}
-	
+
 				if ( $children ) {
 					$html .= $children;
 				}
-	
+
 				$html .= '</ul>';
 			}
 
@@ -123,7 +123,7 @@ if ( ! function_exists( 'nmped_add_search_to_main_nav' ) ) {
 				if ( Avada()->settings->get( 'main_nav_search_icon' ) ) {
 
 					$items .= '<li class="fusion-custom-menu-item fusion-main-menu-search">';
-						$items .= '<a class="fusion-main-menu-icon" aria-label="Search" alt="Search"></a>';
+						$items .= '<a class="fusion-main-menu-icon" tabindex="0" aria-label="Search" alt="Search"></a>';
 						$items .= '<div class="fusion-custom-menu-item-contents">';
 							$items .= get_search_form( false );
 						$items .= '</div>';
