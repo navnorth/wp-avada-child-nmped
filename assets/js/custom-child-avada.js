f75ff6e86d542b761236fb2c4db130805e2e56ec
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-12-19 02:29:21 +0800

    show search when pressing enter key on search icon

--- assets/js/custom-child-avada.js
+++ assets/js/custom-child-avada.js
@@ -4,6 +4,12 @@
         $("p.widget-title").each(function() {
             $(this).prop("style")["font-size"] && $(this).attr("data-inline-fontsize", !0), $(this).prop("style")["font-size"] && $(this).attr("data-inline-lineheight", !0), $(this).attr("data-fontsize", parseInt($(this).css("font-size"))), $(this).attr("data-lineheight", parseInt($(this).css("line-height")));
         });
+        $('.fusion-main-menu .fusion-main-menu-search a').on('keydown', function(e){
+            if (e.which=="13") {
+                e.preventDefault();
+                $('.fusion-main-menu .fusion-main-menu-search a').trigger('click');
+            }
+        });
     });
 
     //Missing Visible Focus fixes for when focus on image - adding tabindex
