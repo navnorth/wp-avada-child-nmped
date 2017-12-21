79ae87e1ac54465c453c5efb70e947a296d6d715
joe hobson <joe@navigationnorth.com>
2017-12-21 14:11:09 -0600

    comment w/ whitelist format, disable for G+ share

--- assets/js/external.js
+++ assets/js/external.js
@@ -2,7 +2,12 @@
   var recentUrls, urlTest, whitelist, whitelisted;
   urlTest = /^(?:http|https|ftp):\/\/([^\/]+)/;
   recentUrls = {};
-  whitelist = [/^.*\.navigationnorth\.com$/];
+  /* basic format for domains in the whitelist: /^domainname\.com$/  -- no quotes around each item in the array */
+  whitelist = [
+                 /^.*navigationnorth\.com$/,
+                 /^plus\.google\.com$/,
+                 /^ped\.state\.nm\.us$/
+              ];
   whitelisted = function(hostname) {
     return _.any(whitelist, function(matcher) {
       return matcher.test(hostname);
@@ -16,14 +21,14 @@
     matches = href.match(urlTest);
     checkMatches = matches && matches[1] !== window.location.host && matches[1] !== window.location.hostname;
     checkOverride = $this.hasClass('-external-no-check') || recentUrls[href] || whitelisted(this.hostname);
-    
+
     if (checkMatches && !checkOverride) {
       e.preventDefault();
-      
+
       if (!$('#dataConfirmModal').length) {
               $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"></div><div class="modal-body"></div><div class="modal-footer"><button class="btn btn-primary btn-white" data-dismiss="modal" aria-hidden="true">No</button><button class="btn btn-primary btn-yellow" tabindex="1" id="dataConfirmOK">Proceed</button></div></div>');
       }
-      
+
       var $html = '<h4>You are about to leave the NMPED website.</h4>'
       $html += '<p>The link you clicked is NOT part of the New Mexico Public Education website.</p>'
       $html += '<p class="href">' + href + '</p>'
