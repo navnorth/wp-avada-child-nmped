f29b4b8eca3b3203d09258f4240685718595cf1d
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2017-12-27 23:20:22 +0800

    display fallback image on news and search results

--- templates/sidebar-1.php
+++ templates/sidebar-1.php
@@ -17,13 +17,15 @@ $sticky_sidebar = in_array( 'fusion-sticky-sidebar', apply_filters( 'fusion_side
 ?>
 <aside id="sidebar" role="complementary" <?php Avada()->layout->add_class( 'sidebar_1_class' ); ?> <?php Avada()->layout->add_style( 'sidebar_1_style' ); ?> <?php Avada()->layout->add_data( 'sidebar_1_data' ); ?>>
 	<?php
-	// Display thumbnail on the top side navigation
-	if (has_post_thumbnail($post->ID)) {
-		$post_thumbnail = get_the_post_thumbnail($post->ID);
-		echo $post_thumbnail;
-	} else {
-		echo '<img src="'.get_stylesheet_directory_uri().'/assets/images/nmped.png" class="default-post-thumbnail size-post-thumbnail" alt="'.get_the_title($post->ID).'" />';
-	}
+	$sidebar_image = '<img src="'.get_stylesheet_directory_uri().'/assets/images/nmped.png" class="default-post-thumbnail size-post-thumbnail" alt="'.get_the_title($post->ID).'" />';
+	if (get_queried_object()) {
+		// Display thumbnail on the top side navigation
+		if (has_post_thumbnail($post->ID)) {
+			$post_thumbnail = get_the_post_thumbnail($post->ID);
+			$sidebar_image = $post_thumbnail;
+		}
+	} 
+	echo $sidebar_image;
 	?>
 	<?php if ( $sticky_sidebar ) : ?>
 		<div class="fusion-sidebar-inner-content">
