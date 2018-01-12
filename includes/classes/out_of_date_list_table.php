75b64ce72d7331c1eb142672c7120ea5e90b7688
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2018-01-13 03:37:11 +0800

    display out of date content data according to mockup

--- includes/classes/out_of_date_list_table.php
+++ includes/classes/out_of_date_list_table.php
@@ -36,11 +36,11 @@ class Out_Of_Date_List_Table extends WP_List_Table{
     */
     function get_columns() {
         return $columns= array(
-           'col_id'=>__('ID'),
-           'col_title'=>__('Name'),
-           'col_url'=>__('Url'),
-           'col_modified_date'=>__('Post_Modified'),
-           'col_author'=>__('Author')
+           /*'ID'=>__('ID'),*/
+           'post_title'=>__(''),
+           /*'post_url'=>__('Url'),*/
+           'post_modified'=>__(''),
+           /*'post_author_name'=>__('')*/
         );
     }
     
@@ -52,12 +52,27 @@ class Out_Of_Date_List_Table extends WP_List_Table{
         return $sortable = array();
     }
     
+     public function get_hidden_columns() {
+        return array();
+    }
+    
+    function column_default( $item, $column_name ) {
+        switch( $column_name ) { 
+            case 'ID':
+            case 'post_title':
+            case 'post_url':
+            case 'post_modified':
+            case 'post_author_name':
+                return $item->$column_name;
+            default:
+                return print_r( $item, true ) ; //Show the whole array for troubleshooting purposes
+        }
+    }
+    
     /**
     * Prepare the table with different parameters, pagination, columns and table elements
     */
     function prepare_items() {
-        global $wpdb, $_wp_column_headers;
-        $screen = get_current_screen();
         
         $args = array(
                     'post_type' => array( 'post', 'page' ),
@@ -112,8 +127,9 @@ class Out_Of_Date_List_Table extends WP_List_Table{
          //The pagination links are automatically built according to those parameters
    
         /* -- Register the Columns -- */
-         $columns = $this->get_columns();
-         $_wp_column_headers[$screen->id]=$columns;
+        $columns = $this->get_columns();
+         
+        $this->_column_headers = array($columns);
    
         /* -- Fetch the items -- */
         $outPosts = new WP_Query(
@@ -122,7 +138,98 @@ class Out_Of_Date_List_Table extends WP_List_Table{
                 $args
             )
         );
+        
+        $results = $outPosts->posts;
+        
+        foreach($results as $result){
+            $author = get_userdata($result->post_author);
+            $author_url = get_author_posts_url($result->post_author);
+            
+            $author_name = $author->display_name;
+            if ($author->first_name!=="" || $author->last_name!=="")
+                $author_name = $author->first_name . " " . $author->last_name;
+            
+            $result->post_author_name = $author_name;
+            $result->post_url = esc_url(get_permalink($result->ID));
+            $result->author_url = $author_url;
+        }
+        
         $this->items = $outPosts->posts;
+    }
+    
+    /**
+    * Display the rows of records in the table
+    * @return string, echo the markup of the rows
+    */
+    function display_rows() {
+   
+        //Get the records registered in the prepare_items method
+        $records = $this->items;
+   
+        //Get the columns registered in the get_columns and get_sortable_columns methods
+        list( $columns, $hidden ) = $this->get_column_info();
+   
+        //Loop for each record
+        if(!empty($records)){
+            foreach($records as $rec){
+        
+                //Open the line
+                echo '<tr id="record_'.$rec->ID.'">';
+           
+                    foreach ( $columns as $column_name => $column_display_name ) {
+   
+                        //Style attributes for each col
+                        $class = "class='$column_name column-$column_name'";
+                        $style = "";
+                        if ( in_array( $column_name, $hidden ) ) $style = ' style="display:none;"';
+                            $attributes = $class . $style;
+               
+                        //edit link
+                        $editlink  = admin_url('post.php?post='.(int)$rec->ID.'&action=edit');
+               
+                        //Display the cell
+                        switch ( $column_name ) {
+                            case "ID":
+                                echo '<td '.$attributes.'>'.stripslashes($rec->ID).'</td>';
+                                break;
+                            case "post_title":
+                                echo '<td '.$attributes.'><a href="'.$editlink.'">'.stripslashes($rec->post_title).'</a><br/>'.$rec->post_url.'</td>';
+                                break;
+                            case "post_url":
+                                echo '<td '.$attributes.'>'.$rec->post_url.'</td>';
+                                break;
+                           case "post_modified":
+                                $post_mod_time = strtotime($rec->post_modified);
+                                echo '<td '.$attributes.'>'.date( "M jS Y", $post_mod_time ).'<br/><a href="'.$rec->author_url.'" target="_blank">'.stripslashes($rec->post_author_name).'</a></td>';
+                                break;
+                           case "post_author_name":
+                                echo '<td '.$attributes.'>'.stripslashes($rec->post_author_name).'</td>';
+                                break;
+                        }
+                    }
+           
+                 //Close the line
+                 echo '</tr>';
+            }
+        }
+    }
+    
+    public function no_items() {
+        _e( 'No contents found.', 'wp-avada-child-nmped' );
+    }
+    
+    /**
+    * Method for name column
+    *
+    * @param array $item an array of DB data
+    *
+    * @return string
+    */
+    function column_name( $item ) {
+    
+        $title = '<strong>' . $item['name'] . '</strong>';
+    
+        return $title;
     }
 }
 
